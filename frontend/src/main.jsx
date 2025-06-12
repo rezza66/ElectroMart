f2365a1c0669e437ac28e784fda8f1006f8dbf27
reza pratama <rezap9303@gmail.com>
2025-06-12 12:28:07 +0900

    modified docker

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/electro-frontend">
         <App />
       </BrowserRouter>
     </PersistGate>
